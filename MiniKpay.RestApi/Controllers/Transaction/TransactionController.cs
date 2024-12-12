c2a0ef6ab881de3bec273f56ec989b45aa2ac366
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-12 21:14:57 +0630

    Add project files.

---
+++ MiniKpay.RestApi/Controllers/Transaction/TransactionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniKpay.RestApi.Controllers.Transaction
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+    }
+}
