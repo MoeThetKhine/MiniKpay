5722e35533a1583d9bb7b291a4a27fa5903ae1e4
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-13 19:10:59 +0630

    -

--- MiniKpay.Domain/Models/Transaction/TransactionRequestModel.cs
+++ MiniKpay.Domain/Models/Transaction/TransactionRequestModel.cs
@@ -1,13 +1,12 @@
-﻿namespace MiniKpay.Domain.Models.Transaction
+﻿namespace MiniKpay.Domain.Models.Transaction;
+
+public class TransactionRequestModel
 {
-    public class TransactionRequestModel
-    {
-        public string SenderMobileNo { get; set; } = null!;
+    public string SenderMobileNo { get; set; } = null!;
 
-        public string ReceiverMobileNo { get; set; } = null!;
+    public string ReceiverMobileNo { get; set; } = null!;
 
-        public decimal? Amount { get; set; }
+    public decimal? Amount { get; set; }
 
-        public string? Notes { get; set; }
-    }
+    public string? Notes { get; set; }
 }
