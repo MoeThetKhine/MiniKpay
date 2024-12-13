c5a82e72693b3b08fc6426e97831f2f3d6970d85
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-13 13:03:59 +0630

    Feat : Add GetTransactionByPhoneNumberAsync

--- MiniKpay.Domain/Features/Transaction/TransactionService.cs
+++ MiniKpay.Domain/Features/Transaction/TransactionService.cs
@@ -44,5 +44,39 @@ namespace MiniKpay.Domain.Features.Transaction
                 return Result<List<TransactionModel>>.SystemError(ex.Message);
             }
         }
+
+        public async Task<Result<List<TransactionModel>>> GetTransactionByPhoneNumberAsync(string phoneNumber)
+        {
+            Result<List<TransactionModel>> response;
+
+            try
+            {
+                var transactions = _db.TblTransactions
+                    .Where(x => x.SenderMobileNo == phoneNumber)
+                    .AsNoTracking();
+
+                if (transactions is null)
+                {
+                    return Result<List<TransactionModel>>.ValidationError("No transactions found with this Phone Number.");
+                }
+
+                var lst = await transactions.Select(x => new TransactionModel()
+                {
+                    TransferId = x.TransferId,
+                    SenderMobileNo = x.SenderMobileNo,
+                    ReceiverMobileNo = x.ReceiverMobileNo,
+                    Amount = x.Amount,
+                    Notes = x.Notes,
+                }).ToListAsync();
+
+
+                return Result<List<TransactionModel>>.Success(lst);
+            }
+            catch (Exception ex)
+            {
+                return Result<List<TransactionModel>>.SystemError(ex.Message);
+            }
+        }
+
     }
 }
