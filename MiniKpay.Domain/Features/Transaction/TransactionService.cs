aa923f9a3f59f55b858f19d196d01849dbc9f8db
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-13 12:20:31 +0630

    Feat : Add GetTransactionAsync in Transaction Service

--- MiniKpay.Domain/Features/Transaction/TransactionService.cs
+++ MiniKpay.Domain/Features/Transaction/TransactionService.cs
@@ -1,6 +1,43 @@
-﻿namespace MiniKpay.Domain.Features.Transaction
+﻿using Microsoft.EntityFrameworkCore;
+using MiniKpay.Database.Models;
+using MiniKpay.Domain.Models;
+using MiniKpay.Domain.Models.Transaction;
+
+namespace MiniKpay.Domain.Features.Transaction
 {
     public class TransactionService
     {
+        private readonly AppDbContext _db;
+
+        public TransactionService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Result<List<TransactionModel>>> GetTransactionAsync()
+        {
+            Result<List<TransactionModel>> response;
+
+            try
+            {
+                var transactions = _db.TblTransactions.AsNoTracking();
+
+                var lst = await transactions.Select(x=> new TransactionModel()
+                {
+                    TransferId = x.TransferId,
+                    SenderMobileNo = x.SenderMobileNo,
+                    ReceiverMobileNo = x.ReceiverMobileNo,
+                    Amount = x.Amount,
+                    Notes = x.Notes,
+                }).ToListAsync();
+
+
+                return Result<List<TransactionModel>>.Success(lst);
+            }
+            catch (Exception ex)
+            {
+                return Result<List<TransactionModel>>.SystemError(ex.Message);
+            }
+        }
     }
 }
