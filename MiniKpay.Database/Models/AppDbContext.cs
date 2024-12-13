c53c8d5dc285072ea15afd2973b8ab865886bcb0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-13 17:21:06 +0630

    -

--- MiniKpay.Database/Models/AppDbContext.cs
+++ MiniKpay.Database/Models/AppDbContext.cs
@@ -2,9 +2,6 @@
 
 public partial class AppDbContext : DbContext
 {
-    //public AppDbContext()
-    //{
-    //}
 
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
