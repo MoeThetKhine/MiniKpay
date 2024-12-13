aa769b48e9e97df4ac7ece548bd1f28e52233b06
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-13 12:48:37 +0630

    Feat : Add GetTransactionAsync in Controller

--- MiniKpay.RestApi/Controllers/Transaction/TransactionController.cs
+++ MiniKpay.RestApi/Controllers/Transaction/TransactionController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MiniKpay.Domain.Features.Transaction;
 
 namespace MiniKpay.RestApi.Controllers.Transaction
 {
@@ -7,5 +8,30 @@ namespace MiniKpay.RestApi.Controllers.Transaction
     [ApiController]
     public class TransactionController : ControllerBase
     {
+        private readonly TransactionService _service;
+
+        public TransactionController(TransactionService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTransactionAsync()
+        {
+            try
+            {
+                var lst = await _service.GetTransactionAsync();
+                return Ok(lst);
+            }
+            catch(Exception ex) 
+            {
+                return StatusCode(500, new
+                {
+                    error = ex.Message
+                });
+            }
+        }
+
+       
     }
 }
