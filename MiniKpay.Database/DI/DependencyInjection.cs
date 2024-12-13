ddd57e26eebb7fa328cc3092cbbdcb426c6a3792
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-13 11:45:30 +0630

    Format Code

--- MiniKpay.Database/DI/DependencyInjection.cs
+++ MiniKpay.Database/DI/DependencyInjection.cs
@@ -1,20 +1,19 @@
-﻿namespace MiniKpay.Database.DI
+﻿namespace MiniKpay.Database.DI;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration configuration)
+        services.AddDbContext<AppDbContext>(opt =>
         {
-            services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseSqlServer(configuration.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+            opt.UseSqlServer(configuration.GetConnectionString("DbConnection"));
+        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
-        //    services.AddScoped<DepositWithdrawService>();
-        //    services.AddScoped<WalletService>();
-        //    services.AddScoped<TransactionService>();
+    //    services.AddScoped<DepositWithdrawService>();
+    //    services.AddScoped<WalletService>();
+    //    services.AddScoped<TransactionService>();
 
-            return services;
-        }
+        return services;
     }
- 
 }
+
