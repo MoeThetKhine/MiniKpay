61c6e7f01faa606829e86dbf1cf7cd53aeae1c5c
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-13 12:53:52 +0630

    Feat : Add TransactionRequestModel

---
+++ MiniKpay.Domain/Models/Transaction/TransactionRequestModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MiniKpay.Domain.Models.Transaction
+{
+    public class TransactionRequestModel
+    {
+        public string SenderMobileNo { get; set; } = null!;
+
+        public string ReceiverMobileNo { get; set; } = null!;
+
+        public decimal? Amount { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
