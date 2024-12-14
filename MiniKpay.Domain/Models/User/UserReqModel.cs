74146c6ce2d03226307701333360a4d72aa9391b
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 14:14:28 +0630

    -

--- MiniKpay.Domain/Models/User/UserReqModel.cs
+++ MiniKpay.Domain/Models/User/UserReqModel.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MiniKpay.Domain.Models.User;
 
-namespace MiniKpay.Domain.Models.User
+public class UserReqModel
 {
-    public class UserReqModel
-    {
-        public string UserName { get; set; } = null!;
+    public string UserName { get; set; } = null!;
 
-        public string MobileNumber { get; set; } = null!;
+    public string MobileNumber { get; set; } = null!;
 
-        public string PinCode { get; set; } = null!;
+    public string PinCode { get; set; } = null!;
 
-        public decimal? Balance { get; set; }
-    }
+    public decimal? Balance { get; set; }
+
 }
