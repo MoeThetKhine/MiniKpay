87b0542cb27e76ea96c32a5768534e0294955dcc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 14:14:11 +0630

    -

--- MiniKpay.Domain/Models/User/UserResponseModel.cs
+++ MiniKpay.Domain/Models/User/UserResponseModel.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MiniKpay.Database.Models;
+﻿namespace MiniKpay.Domain.Models.User;
 
-namespace MiniKpay.Domain.Models.User
+public class UserResponseModel
 {
-    public class UserResponseModel
-    {
-     public TblWallet? Wallet{ get; set; }
-    }
+    public TblWallet? Wallet{ get; set; }
 }
