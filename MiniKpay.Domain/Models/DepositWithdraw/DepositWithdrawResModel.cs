151ceaeb96b76d50491928b121c05fa7e17b3cf7
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 14:22:29 +0630

    -

--- MiniKpay.Domain/Models/DepositWithdraw/DepositWithdrawResModel.cs
+++ MiniKpay.Domain/Models/DepositWithdraw/DepositWithdrawResModel.cs
@@ -2,12 +2,8 @@
 
 public class DepositWithdrawResModel
 {
- 
     public string MobileNumber { get; set; } = null!;
-
     public DateTime Date { get; set; }
-
     public decimal Amount { get; set; }
-
     public string TransactionType { get; set; } = null!;
 }
