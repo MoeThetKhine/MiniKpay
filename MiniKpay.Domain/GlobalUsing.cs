ffd2fae6ceff33f5edb24bccc15400794ba968e8
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-14 14:12:03 +0630

    Feat : Add Global Using

--- MiniKpay.Domain/GlobalUsing.cs
+++ MiniKpay.Domain/GlobalUsing.cs
@@ -3,3 +3,4 @@ global using MiniKpay.Database.Models;
 global using MiniKpay.Domain.Models;
 global using MiniKpay.Domain.Models.Transaction;
 global using MiniKpay.Domain.Models.DepositWithdraw;
+global using MiniKpay.Domain.Models.User;
